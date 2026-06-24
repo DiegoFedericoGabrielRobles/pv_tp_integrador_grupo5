f2cccc8a8bfccb3ca78ca70de4b62f7438fa0f79
Diego Robles <111714917+DiegoFedericoGabrielRobles@users.noreply.github.com>
2026-06-24 01:23:50 -0300

    ADD: Implementa autenticación y carga inicial de clientes desde FakeStoreAPI

--- Trabajo_Final_Integrador/src/views/ListaClientes.jsx
+++ Trabajo_Final_Integrador/src/views/ListaClientes.jsx
@@ -1,5 +1,45 @@
+import { useState, useEffect } from "react";
+
+import CircularProgress from "@mui/material/CircularProgress";
+import Alert from "@mui/material/Alert";
+
 export const ListaClientes = () => {
+    const [clientes, setClientes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [busqueda, setBusqueda] = useState("");
+
+    const obtenerClientes = async () => {
+        try {
+            const respuesta = await fetch( "https://fakestoreapi.com/users");
+            const datos = await respuesta.json();
+            setClientes(datos);
+            setLoading(false);
+        } catch (error) {
+            setError(true);
+            setLoading(false);
+        }
+
+    };
+
+    useEffect(() => {
+        obtenerClientes();
+    }, []);
+
+    if (loading) {
+        return <CircularProgress />;
+    }
+
+    if (error) {
+        return (
+            <Alert severity="error"> Error al cargar los clientes. </Alert>
+        );
+    }
+
     return (
-        <h1>LISTA DE CLIENTES</h1>
+        <>
+            <h1>Lista de Clientes</h1>
+            <p>Cantidad de clientes: {clientes.length}</p>
+        </>
     );
 };
