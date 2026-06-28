37903459594fe4282fbef7671ab252539f1245e0
Diego Robles <111714917+DiegoFedericoGabrielRobles@users.noreply.github.com>
2026-06-27 22:39:19 -0300

    FEAT: Agrega ciudad, teléfono, usuario y contraseña al formulario de clientes

--- Trabajo_Final_Integrador/src/components/common/FormularioCliente.jsx
+++ Trabajo_Final_Integrador/src/components/common/FormularioCliente.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import { crearCliente } from "../../services/clienteService";
+
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -8,34 +10,43 @@ import Button from "@mui/material/Button";
 import Alert from "@mui/material/Alert";
 
 export const FormularioCliente = () => {
+    const [mensajeExito, setMensajeExito] = useState("");
     const [nombre, setNombre] = useState("");
     const [apellido, setApellido] = useState("");
     const [email, setEmail] = useState("");
-    const [mensajeExito, setMensajeExito] = useState("");
+    const [ciudad, setCiudad] = useState("");
+    const [telefono, setTelefono] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     const guardarCliente = async () => {
         const nuevoCliente = {
             email: email,
+            username: username,
+            password: password,
+            phone: telefono,
 
             name: {
                 firstname: nombre,
                 lastname: apellido
+            },
+
+            address: {
+                city: ciudad
             }
         };
 
         try {
-            const respuesta = await fetch("https://fakestoreapi.com/users",
-                {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json"},
-                    body: JSON.stringify(nuevoCliente)
-                });
-
-            const datos = await respuesta.json();
+            const datos = await crearCliente(nuevoCliente);
+            
             setMensajeExito(`Cliente creado correctamente. ID asignado: ${datos.id}`);
             setNombre("");
             setApellido("");
             setEmail("");
+            setTelefono("");
+            setCiudad("");
+            setUsername("");
+            setPassword("");
         }
         catch (error) {
         console.error(error);
@@ -71,6 +82,39 @@ export const FormularioCliente = () => {
                     onChange={(e) => setEmail(e.target.value)}
                 />
 
+                <TextField
+                    fullWidth
+                    label="Telefono"
+                    margin="normal"
+                    value={telefono}
+                    onChange={(e) => setTelefono(e.target.value)}
+                />
+
+                <TextField
+                    fullWidth
+                    label="Ciudad"
+                    margin="normal"
+                    value={ciudad}
+                    onChange={(e) => setCiudad(e.target.value)}
+                />
+
+                <TextField
+                    fullWidth
+                    label="Username"
+                    margin="normal"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+
+                 <TextField
+                    fullWidth
+                    label="Password"
+                    margin="normal"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
                 <Button
                     variant="contained"
                     fullWidth
@@ -85,7 +129,6 @@ export const FormularioCliente = () => {
                         {mensajeExito}
                     </Alert>
                 )}
-
             </CardContent>
         </Card>
     );
