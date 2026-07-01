1a8b708e6870d8b7f2f79e11ee167ddd2ba0e958
Diego Robles <111714917+DiegoFedericoGabrielRobles@users.noreply.github.com>
2026-07-01 18:14:17 -0300

    MOD: Muestra el Footer solo después del inicio de sesión

--- Trabajo_Final_Integrador/src/components/layout/Nav.jsx
+++ Trabajo_Final_Integrador/src/components/layout/Nav.jsx
@@ -146,7 +146,7 @@ export const Nav = ({children}) => {
                     <Box sx={{ flexGrow: 1 }}>
                         {children}
                     </Box>
-                    <Footer />
+                    {admin && <Footer />}
                 </Box>
         </Box> 
     );
